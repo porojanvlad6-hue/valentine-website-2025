bb724095c91093b53162fa3775eb9cf6c088ea1f
porojanvlad6-hue <porojanvlad6@gmail.com>
2026-01-29 10:43:04 -1200

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: ", /https://res.cloudinary.com/dtsh4ve6c/video/upload/v1769724971/GYA_-_Pe_limba_noastra___Official_Video_twhedd.mp3/ Music streaming URL
+        musicUrl: ", https://res.cloudinary.com/dtsh4ve6c/video/upload/v1769725910/GYA_-_Pe_limba_noastra___Official_Video_e2brxs.mp3/ Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
